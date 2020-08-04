d18118c75d28f629dafe4dbec38091470c5174ce
hossam eldien mokhtar <69170538+hossameldien1984@users.noreply.github.com>
2020-08-04 12:27:00 +0200

    Update mobile app retention time 3 days avg.sql
    Solution mobile app retention time 3 days avg

--- mobile app retention time 3 days avg.sql
+++ mobile app retention time 3 days avg.sql
@@ -1,14 +1,4 @@
-/*
-Please review the dataset schema provided on the 'data1' sheet.
- The sheet contains an extract from SQL table.
-  Please create a SQL query that presents retention values for 
-  each application on each hardware_platform and for each day, 
-  but as an average value of 3 days preceding each given day.
-Desired final schema is:
- date, application, hardware_platform, retention_day_1_3days_avg,
-  retention_day_3_3days_avg, retention_day_7_3days_avg
-*/
-use sales;
+
 SELECT 
     da11.date,
     da11.application,
